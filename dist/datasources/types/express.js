62588a2ae12c27bd1c05812ad9c560d0fc7a903e
YaswanthBonumaddi9 <yaswanth@godspeed.systems>
2023-09-05 22:30:11 +0530

    first commit

---
+++ dist/datasources/types/express.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const godspeed_plugin_express_as_http_1 = require("godspeed-plugin-express-as-http");
+exports.default = godspeed_plugin_express_as_http_1.ExpressDataSource;
+//# sourceMappingURL=express.js.map
