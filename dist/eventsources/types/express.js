ab58694ddd9b3292ef9d87abf4ce74bb6f46ab1e
YaswanthBonumaddi9 <143052514+YaswanthBonumaddi9@users.noreply.github.com>
2023-09-06 13:50:12 +0530

    Delete dist directory

--- dist/eventsources/types/express.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const plugins_express_as_http_1 = require("@godspeedsystems/plugins-express-as-http");
-exports.default = plugins_express_as_http_1.ExpressEventSource;
-//# sourceMappingURL=express.js.map
